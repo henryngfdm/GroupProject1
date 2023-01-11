dc47869d37e2a8e7ff8105678cf6b7c22b5b754b
henryforesight <114891336+henryngfdm@users.noreply.github.com>
2023-01-11 11:52:16 +1100

    mongo

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+import pandas as pd
 
 app = Flask(__name__)
+
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throu oghout the tutorial
+   return client['project1']
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/get")
+def call():
+    dbname = get_database()
+    collection_name = dbname["opportunites"]
+    item_details = collection_name.find()
+    items_df = pd.DataFrame(item_details)
+    items_df = items_df.drop(columns=['_id'])
+    return items_df.head(4).to_json()
 
 if __name__ == "__main__":
     app.run(debug=True)
